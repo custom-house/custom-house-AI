02027067ae61a73e78fb9f127971b2b859e991bd
Yang Kyung Hun <ygh1254@hanyang.ac.kr>
2022-11-24 15:32:41 +0900

    [feat] Split Object
    Split appliance objects into small pieces

--- swe.py
+++ swe.py
@@ -1,8 +1,19 @@
 import pandas as pd
+import numpy as np
 
 routine = pd.read_csv('Routine.csv')
-routine = routine.drop(['_id', 'command', 'timer', 'routine_name'], axis = 1)
+# routine = routine.drop(['_id', 'command', 'timer', 'routine_name'], axis = 1)
 
 # header_list = routine.columns.tolist()
 
-print(routine)
+appliance = routine['Appliance']
+x = appliance.values
+appliances = []
+
+for idx in range(len(x)):
+    appliances.append(x[idx].rstrip(']}').lstrip('[{').split('},{'))
+
+for idx in range(len(appliances)):
+    for index in range(len(appliances[idx])):
+        print(appliances[idx][index])
+    print()
