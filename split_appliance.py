2e30e631e19aa6d0f2f7a55882f4233dd1094a9e
Yang Kyung Hun <ygh1254@hanyang.ac.kr>
2022-11-26 22:28:31 +0900

    [feat] DB can be analyzed
    Create details.csv which is form to be analyzed

--- split_appliance.py
+++ split_appliance.py
@@ -13,8 +13,6 @@ for idx in range(len(appliances)):
     details_array.append(details)
     details = []
 
-df_appliance = pd.DataFrame()
-
 df_detail = pd.DataFrame({'appliance_id': [], 'appliance_name': [], 'power': [], 'set_ch': [], 'set_vol': [],
                           'briefing': [], 'anti_wrinkle': [], 'eco_dry': [], 'set_time': [], 'set_temp': [],
                           'fire_power': [], 'wind_power': [], 'set_mod': [], 'burner': [], 'preserve': [],
@@ -25,6 +23,6 @@ for i in range(len(details_array)):
     for idx in range(len(details_array[i])):
         header = details_array[i][idx][0].strip('"')
         value = details_array[i][idx][1].strip('"')
-        print(header, value)
-        tempdf = pd.DataFrame({}, index=[idx])
-    print()
+        df_detail.at[i, header] = value
+
+df_detail.to_csv('details.csv')
